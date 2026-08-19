99a1f4ee6b0fe0449c3591712ba88e6f6080e19c
diksha-patil-ac <diksha.patil.cmaug26@gmail.com>
2026-08-19 15:15:14 +0530

    Delete Logic Building/RectangleArea.java

--- Logic Building/RectangleArea.java
+++
@@ -1,8 +0,0 @@
-class RectangleArea {
-    public static void main(String args[]) {
-        int length = 6;
-	int breadth = 7;
-	int area = length * breadth;
-        System.out.println("Area of Rectangle = " + area);
-    }
-}
