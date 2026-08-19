25fdf0262d959a64b1ff6e1693aabd16903125e0
diksha-patil-ac <diksha.patil.cmaug26@gmail.com>
2026-08-19 15:16:58 +0530

    Add files via upload

---
+++ Logic Building/Assignment_1/LargestNumber.java
@@ -0,0 +1,20 @@
+class LargestNumber {
+	public static void main(String args[]) {
+		int num1 = 12;
+		int num2 = 24;
+		int num3 = 15;
+		
+		if(num1>num2 && num1>num3) {
+			System.out.println(num1 + " is the largest");
+		} else if(num2>num3) {
+			System.out.println(num2 + " is the largest");
+		} else {
+			System.out.println(num3 + " is the largest");
+		}
+	}
+}
+
+
+		
+
+		
