d0c74aed3e93da8596c98d4013f3955d93f7dc6a
diksha-patil-ac <diksha.patil.cmaug26@gmail.com>
2026-09-15 01:51:09 +0530

    Add files via upload

---
+++ OOPJ/Assignment_1/SalesPerson.java
@@ -0,0 +1,14 @@
+
+class SalesPerson extends Employee
+{
+    public SalesPerson(String n, String a, int ag, boolean g, float salary)
+    {
+        super(n, a, ag, g, salary);
+    }
+
+    @Override
+    public String getDesignation()
+    {
+        return "Sales Person";
+    }
+}
