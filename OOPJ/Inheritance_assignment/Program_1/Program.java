ecc959bce2657b494640e8edb15aea328e4da89b
diksha-patil-ac <diksha.patil.cmaug26@gmail.com>
2026-09-19 18:05:42 +0530

    Add files via upload

---
+++ OOPJ/Inheritance_assignment/Program_1/Program.java
@@ -0,0 +1,29 @@
+package Exercise_1;
+
+class parent
+{
+	void parentMethod()
+	{
+		System.out.println("This is parent class");;
+	}
+}
+
+class child extends parent
+{
+	void childMethod()
+	{
+		System.out.println("This is child class");
+	}
+}
+
+public class Program 
+{
+	public static void main(String []args)
+	{
+		parent p = new parent();
+		child c = new child();
+		p.parentMethod();
+		c.childMethod();
+		c.parentMethod();
+	}
+}
