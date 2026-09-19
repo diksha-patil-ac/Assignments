a76df3f0610e9bba5ab5fa017f6115b5a5e912a5
diksha-patil-ac <diksha.patil.cmaug26@gmail.com>
2026-09-19 18:30:17 +0530

    Add files via upload

---
+++ OOPJ/String_assignment/Program_5.java
@@ -0,0 +1,16 @@
+
+public class Program_5 {
+
+    public static void main(String[] args) {
+
+        String string = "The quick brown fox jumps over the lazy dog.";
+
+        System.out.println("Original string: " + string);
+
+        String newString = string.replaceAll("fox", "cat");
+
+        System.out.println("New String: " + newString);
+
+    }
+
+}
---
+++ OOPJ/String_assignment/Program_6.java
@@ -0,0 +1,16 @@
+
+public class Program_6 {
+
+    public static void main(String[] args) {
+
+        String string = "The Quick BroWn FoX!";
+
+        System.out.println("Original String: " + string);
+
+        String upperString = string.toUpperCase();
+
+        System.out.println("String in uppercase: " + upperString);
+
+    }
+
+}
